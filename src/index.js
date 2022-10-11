7cfa35adb03004ae7113d791086fa2828209032a
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-10-11 15:11:52 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./i18next/i18n"
+import "./i18next/i18n";
 import App from "./App";
 import { GlobalStyle } from "./GlobalStyles";
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,6 +10,3 @@ root.render(
     <App />
   </>
 );
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
