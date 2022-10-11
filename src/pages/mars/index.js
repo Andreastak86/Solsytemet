e4632102113ec700d116d2482cc616c38ff15b3d
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2022-10-11 15:22:50 +0200

    Adding styles-component

--- src/pages/mars/index.js
+++ src/pages/mars/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Mars from "./img/mars.jpg";
+import { Button, Paragraf, Headder } from "./styles";
 
 const index = () => {
   return (
     <section id='Mars'>
       <div className='container mars__container'>
         <article>
-          <h1>Mars</h1>
+          <Headder>Mars</Headder>
           <div className='mars-img'>
             <img src={Mars} alt='Planet Mars' />
           </div>
-          <p>
+          <Paragraf>
             Mars er den fjerde planeten regnet fra Sola. Den er den syvende
             største planeten i solsystemet og har litt over halvparten så stor
             omkrets som Jorda. Den er i likhet med Merkur, Venus og Jorda en
@@ -19,12 +20,12 @@ const index = () => {
             for «solar day» (soldøgn). Mars kan ha vært mer jordlignende for 3,5
             milliarder år siden. Formasjoner på overflaten tyder på at vann har
             rent der tidligere, mens i dag er overflaten tørrlagt.
-            <span className='ref'>
+            <p className='ref'>
               Aksnes, Kaare; Hammerstrøm, Maria: Mars i Store norske leksikon på
               snl.no. Hentet 11. oktober 2022 fra{" "}
-              <a href='https://snl.no/Mars'>SNL</a>
-            </span>
-          </p>
+              <Button href='https://snl.no/Mars'>SNL</Button>
+            </p>
+          </Paragraf>
         </article>
       </div>
     </section>
---
+++ src/pages/mars/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: red;
+`;
+
+export const Paragraf = styled.p`
+  color: red;
+  font-size: 30px;
+`;
+
+export const Headder = styled.h1`
+  color: orange;
+`;
