374fd81f8f3aff028f026fb1345f2a8e88d48895
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2022-10-12 10:42:19 +0200

    changes

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,51 +1,51 @@
-import React from "react";
-import "./index.css";
-import { useRef } from "react";
-import emailjs from "@emailjs/browser";
-import { Container, Options, Button } from "./styles";
+// import React from "react";
+// import "./index.css";
+// import { useRef } from "react";
+// import emailjs from "@emailjs/browser";
+// import { Container, Options, Button } from "./styles";
 
-const Contact = () => {
-  const form = useRef();
+// const Contact = () => {
+//   const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+//   const sendEmail = (e) => {
+//     e.preventDefault();
 
-    emailjs.sendForm(
-      "service_ll2alhu",
-      "template_n8q0gqf",
-      form.current,
-      "GyOKH1R0saYvMD1DZ"
-    );
-    e.target.reset();
-  };
+//     emailjs.sendForm(
+//       "service_ll2alhu",
+//       "template_n8q0gqf",
+//       form.current,
+//       "GyOKH1R0saYvMD1DZ"
+//     );
+//     e.target.reset();
+//   };
 
-  return (
-    <Container>
-      <h2>Kontakt Oss</h2>
-      <Options>
-        <div>
-          <article className='contact__option'>
-            <form ref={form} onSubmit={sendEmail}>
-              <input type='text' name='name' placeholder='Ditt Navn' required />
-              <input
-                type='email'
-                name='email'
-                placeholder='Din Mailadresse'
-                required
-              />
-              <textarea
-                name='message'
-                rows='10'
-                placeholder='Din Melding'
-                required
-              ></textarea>
-              <Button type='submit'>Send Melding</Button>
-            </form>
-          </article>
-        </div>
-      </Options>
-    </Container>
-  );
-};
+//   return (
+//     <Container>
+//       <h2>Kontakt Oss</h2>
+//       <Options>
+//         <div>
+//           <article className='contact__option'>
+//             <form ref={form} onSubmit={sendEmail}>
+//               <input type='text' name='name' placeholder='Ditt Navn' required />
+//               <input
+//                 type='email'
+//                 name='email'
+//                 placeholder='Din Mailadresse'
+//                 required
+//               />
+//               <textarea
+//                 name='message'
+//                 rows='10'
+//                 placeholder='Din Melding'
+//                 required
+//               ></textarea>
+//               <Button type='submit'>Send Melding</Button>
+//             </form>
+//           </article>
+//         </div>
+//       </Options>
+//     </Container>
+//   );
+// };
 
-export default Contact;
+// export default Contact;
