fc6144fe307e886d967da88fe74a338edfe82fcc
Ole Håvard Furuseth Bergan <ohfb96@gmail.com>
2022-10-13 08:15:20 +0200

    more info on sun

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
-// import Contact from "./components/contact";
 import FooterBar from "./components/footer";
-// import SetupEksempel from "./components/i18eksempel.js/SetupEksempel";
 import Nav from "./components/navbar";
 import LandingPage from "./pages//LandingPage/index";
 import {
-  // HashRouter as Router,
   Routes,
   Route,
   BrowserRouter,
-  // NavLink
 } from "react-router-dom";
 import Sun from "./pages/sun";
 
--- src/pages/sun/index.js
+++ src/pages/sun/index.js
@@ -1,5 +1,11 @@
 import { t } from "i18next";
-import { SunHeader, SunText, SunTitle } from "./styles";
+import { 
+    SunEarthImage,
+    SunHeader, 
+    SunImgTextFlex, 
+    SunText, 
+    SunTitle 
+} from "./styles";
 
 function Sun() {
     return (
@@ -12,6 +18,13 @@ function Sun() {
             <SunText>
                 {t("Sun.titleSubtext")}
             </SunText>
+            <SunImgTextFlex>
+                <SunEarthImage />
+                <SunText>
+                The Sun doesn't have a "year," per se. But the Sun orbits the center of the Milky Way about every 230 million Earth years, bringing the planets, asteroids, comets, and other objects with it.
+                    {/* {t("")} */}
+                </SunText>
+            </SunImgTextFlex>
         </>
     )
 }
