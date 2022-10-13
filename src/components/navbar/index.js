a5cf6ffe90a1af3f8c83bb8a4e017eeb328c4e70
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-10-13 14:29:14 +0200

    fikset nav, og footer, links er ikke korrekte atm.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,60 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { t } from "i18next";
 import {
   NavBar,
+  NavBar2,
   NavMenuItems,
   NavMenuLink,
   NavMenuList,
-  WebsiteName
+  NavMenuList2,
+  WebsiteName,
 } from "./styles";
 import { Link } from "react-router-dom";
 
 function Nav() {
-  return (
-    <NavBar>
-      <WebsiteName>
-        <NavMenuLink
-        >
-          <Link to='/Solsytemet'>
+  const [active, setActive] = useState();
+  const planetDropDown = (id) => {
+    setActive(!active);
+  };
 
-            {t("Navbar.title")}
-          </Link>
-        </NavMenuLink>
-      </WebsiteName>
-      <NavMenuList>
+  return (
+    <>
+      <NavBar>
+        <WebsiteName>
+          <NavMenuLink>
+            <Link to="/Solsytemet">{t("Navbar.title")}</Link>
+          </NavMenuLink>
+        </WebsiteName>
+        <NavMenuList>
+          <NavMenuItems>
+            <NavMenuLink>
+              <Link to="/Solsytemet/sun">{t("Navbar.mainPage")}</Link>
+            </NavMenuLink>
+          </NavMenuItems>
+          <NavMenuItems>
+            <NavMenuLink>
+              <Link>{t("Navbar.ourSolarsystem")}</Link>
+            </NavMenuLink>
+          </NavMenuItems>
+          <NavMenuItems onClick={planetDropDown}>
+            <NavMenuLink>{t("Navbar.planets")}</NavMenuLink>
+          </NavMenuItems>
+          <NavMenuItems>
+            <NavMenuLink>{t("Navbar.contact")}</NavMenuLink>
+          </NavMenuItems>
+        </NavMenuList>
+      </NavBar>
+      <NavBar2 active={active}>
         <NavMenuItems>
-          <NavMenuLink
-          >
-            <Link to='/Solsytemet/sun'>
-              {t("Navbar.mainPage")}
-            </Link>
+          <NavMenuLink>
+            <Link to="/Solsytemet/mars">Mars</Link>
           </NavMenuLink>
         </NavMenuItems>
         <NavMenuItems>
-          <NavMenuLink
-          >
-            <Link>
-              {t("Navbar.ourSolarsystem")}
-            </Link>
+          <NavMenuLink>
+            <Link to="/Solsytemet/jupiter">Jupiter</Link>
           </NavMenuLink>
         </NavMenuItems>
         <NavMenuItems>
-          <NavMenuLink
-            href="#"
-          >
-            {t("Navbar.planets")}
-          </NavMenuLink>
+          <Link to="/Solsytemet/saturn">Saturn</Link>
         </NavMenuItems>
         <NavMenuItems>
-          <NavMenuLink
-            href="#"
-          >
-            {t("Navbar.contact")}
-          </NavMenuLink>
+          <Link to="/Solsytemet/moon">Moon</Link>
         </NavMenuItems>
-      </NavMenuList>
-    </NavBar>
-  )
+      </NavBar2>
+    </>
+  );
 }
 
 export default Nav;
