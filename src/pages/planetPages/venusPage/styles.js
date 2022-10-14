58d7436ef5d3d17a3dc96e1c78c3e1a4d26c44b6
michhkh <michaelh.kodehode@gmail.com>
2022-10-14 17:42:08 +0200

    Updated styling and responsiveness for planet pages

--- src/pages/planetPages/venusPage/styles.js
+++ src/pages/planetPages/venusPage/styles.js
@@ -3,23 +3,26 @@ import venus from "./img/Venus.png";
 
 export const StyledPageSection = styled.section`
   width: 100%;
-  padding: 10em 0 0em;
+  padding: 11em 0;
   background-color: #000;
   background-image: url(${venus});
   background-repeat: no-repeat;
   background-position: 40% 40%;
-  text-align: left;
   height: 100vh;
   line-height: 1.2;
 
-  @media screen and (min-width: 600px) {
+  @media screen and (min-width: 1200px) {
     line-height: 1.6;
   }
 `;
 
 export const StyledPageContainer = styled.div`
   width: 75%;
-  margin: auto auto;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 export const StyledPageMainWrapper = styled.div`
@@ -28,7 +31,7 @@ export const StyledPageMainWrapper = styled.div`
   flex-direction: column;
   justify-content: flex-start;
 
-  @media screen and (min-width: 600px) {
+  @media screen and (min-width: 768px) {
     flex-direction: row;
     justify-content: space-between;
   }
@@ -37,41 +40,42 @@ export const StyledPageMainWrapper = styled.div`
 export const StyledPageInfoWrapper = styled.div`
   width: ${(props) => (props.info ? "100%" : props.facts && "100%")};
 
-  @media screen and (min-width: 600px) {
-    width: ${(props) => (props.info ? "50%" : props.facts && "30%")};
+  @media screen and (min-width: 768px) {
+    width: ${(props) => (props.info ? "60%" : props.facts && "30%")};
   }
 `;
 
 export const StyledPageFooter = styled.footer`
-  margin-top: 45%;
   color: #fcf2dc;
-
-  @media screen and (min-width: 600px) {
-    margin-top: 12%;
-  }
 `;
 
 export const StyledPageH1 = styled.h1`
   margin: 0;
-  padding: 1em 0;
-  font-size: 1.8rem;
+  padding: 0.5em 0 0.25em;
+  font-size: 1.6rem;
   color: #fcf2dc;
+  line-height: 1;
 
-  @media screen and (min-width: 600px) {
+  @media screen and (min-width: 768px) {
+    font-size: 2.2rem;
+  }
+
+  @media screen and (min-width: 1200px) {
     font-size: 3rem;
   }
 `;
 
 export const StyledPageP = styled.p`
   margin: 0;
-  padding: 0.5em 0;
+  padding: 0.25em 0;
   color: #fcf2dc;
   font-size: ${(props) => (props.footer ? "0.6rem" : "1rem")};
   filter: ${(props) =>
     props.footer ? "none" : "drop-shadow(1px 2px 1px black)"};
 
-  @media screen and (min-width: 600px) {
+  @media screen and (min-width: 768px) {
     font-size: ${(props) => (props.footer ? "0.8rem" : "1.2rem")};
+    padding: 0.5em 0;
   }
 `;
 
@@ -83,7 +87,7 @@ export const StyledPageLi = styled.li`
   color: #fcf2dc;
   padding-bottom: 0.25em;
 
-  @media screen and (min-width: 600px) {
+  @media screen and (min-width: 768px) {
     padding-bottom: 1em;
   }
 `;
